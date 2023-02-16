e5d262c07ac079d24ee2aca9d3d3623a35d28b25
thihaaung <thihaaung7779@gmail.com>
2023-02-16 23:13:00 +0630

    add db setup

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import multer from "multer";
 import helmet from "helmet";
+import morgan from "morgan";
 
 /*config*/
 const __filename = fileURLToPath(import.meta.url);
@@ -34,5 +35,18 @@ const storage = multer.diskStorage({
     cb(null, file.originalname);
   }
 });
-
 const upload = multer({ storage });
+
+/* DB Config */
+const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((error) => {
+    console.log(`${error} did not connect.`);
+  });
