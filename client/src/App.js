081253ddb3dcd236ef8749cf9e69482f6e16193a
thihaaung <thihaaung7779@gmail.com>
2023-02-18 13:03:01 +0630

    Add material ui theme

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,23 @@
-import "./App.css";
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+
+import Home from "pages/home";
+import Profile from "pages/profile";
+import Login from "pages/login";
+import { useState } from "react";
 
 function App() {
-  return <div className="app"></div>;
+  const mode = useState;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
